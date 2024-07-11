5b73f729f423c74f5830b4dd281ebf59c2d30f2e
unknown <369599207@qq.com>
2024-07-11 18:40:40 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<template>
-  <div id="viz" ref="viz" class="neovis-container" :style="styles"></div>
-</template>
+<template>  
+  <div id="viz"></div>
+</template>  
+  
+<script>  
+// 引入neovis.js  
+import NeoVis from 'neovis.js/dist/neovis.js';  
+  
+export default {  
+  name: 'Neo4jVisualization',  
+  mounted() {  
+    this.draw();  
+  },  
+  methods: {  
+    draw() {
+        let neoViz;
+        const config = {
+            containerId: "viz",
+            neo4j: {
+                serverUrl: "bolt://localhost:7687",
+                serverUser: "neo4j",
+                serverPassword: "174235",
+            },
+			initialCypher: "MATCH (n)-[r]->(m) RETURN n, m"
+        };
 
-<script>
-import NeoVis from 'neovis.js';
-
-export default {
-  name: 'NeovisComponent',
-  data() {
-    return {
-      styles: {
-        width: '900px',
-        height: '700px',
-        border: '1px solid lightgray',
-        fontSize: '22pt',
-      },
-      neovisInstance: null,
-    };
-  },
-  mounted() {
-    this.initNeovis();
-  },
-  methods: {
-    initNeovis() {
-      const config = {
-        containerId: 'viz',
-        neo4j: {
-          serverUrl: 'bolt://localhost:7687',
-          serverUser: 'neo4j',
-          serverPassword: '174235',
-        },
-        initialCypher: 'MATCH (n)-[r]->(m) DETACH DELETE r RETURN n, m',
-      };
-
-      this.neovisInstance = new NeoVis(config);
-      this.neovisInstance.render();
-    },
-  },
-};
-</script>
-
-<style scoped>
-.neovis-container {
-  font-family: 'Arial', sans-serif;
-}
+        neoViz = new NeoVis.default(config);
+        neoViz.render();
+    }
+  }  
+};  
+</script>  
+  
+<style scoped>  
+#viz {  
+  width: 900px;  
+  height: 700px;  
+  border: 1px solid lightgray;  
+  font: 22pt arial;  
+}  
 </style>
