bf1695e5a80a2f5ecb5657dd627a6697351a6675
unknown <369599207@qq.com>
2024-07-11 18:30:55 +0800

    neo2 update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="container">
-    <button @click="plus">count+1</button>
-    <p>{{ count }}</p>
-  </div>
+  <div id="viz" ref="viz" class="neovis-container" :style="styles"></div>
 </template>
 
 <script>
-
+import NeoVis from 'neovis.js';
 
 export default {
+  name: 'NeovisComponent',
   data() {
     return {
-      count:1
-    }
+      styles: {
+        width: '900px',
+        height: '700px',
+        border: '1px solid lightgray',
+        fontSize: '22pt',
+      },
+      neovisInstance: null,
+    };
   },
-  methods:{
-    plus(){
-      this.count += 1
-    }
-  }
-}
+  mounted() {
+    this.initNeovis();
+  },
+  methods: {
+    initNeovis() {
+      const config = {
+        containerId: 'viz',
+        neo4j: {
+          serverUrl: 'bolt://localhost:7687',
+          serverUser: 'neo4j',
+          serverPassword: '174235',
+        },
+        initialCypher: 'MATCH (n)-[r]->(m) DETACH DELETE r RETURN n, m',
+      };
+
+      this.neovisInstance = new NeoVis(config);
+      this.neovisInstance.render();
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.neovis-container {
+  font-family: 'Arial', sans-serif;
+}
 </style>
