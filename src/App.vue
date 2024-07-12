0fc25f17270a7281d9a0fd8cfb693dfc2379da12
unknown <369599207@qq.com>
2024-07-12 10:40:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
-<template>  
-  <div id="viz"></div>
-</template>  
-  
-<script>  
-// 引入neovis.js  
-import NeoVis from 'neovis.js/dist/neovis.js';  
-  
-export default {  
-  name: 'Neo4jVisualization',  
-  mounted() {  
-    this.draw();  
-  },  
-  methods: {  
-    draw() {
-        let neoViz;
-        const config = {
-            containerId: "viz",
-            neo4j: {
-                serverUrl: "bolt://localhost:7687",
-                serverUser: "neo4j",
-                serverPassword: "174235",
-            },
-			initialCypher: "MATCH (n)-[r]->(m) RETURN n, m"
-        };
+<script>
+import neo from "./components/neoCom.vue"
+export default {
+  data() {
+    return {
 
-        neoViz = new NeoVis.default(config);
-        neoViz.render();
     }
-  }  
-};  
-</script>  
-  
-<style scoped>  
-#viz {  
-  width: 900px;  
-  height: 700px;  
-  border: 1px solid lightgray;  
-  font: 22pt arial;  
-}  
+  },
+  components: {
+    neo
+  }
+}
+</script>
+
+<template>
+  <neo/>
+</template>
+
+<style>
+
 </style>
---
+++ src/components/neoCom.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="viz"></div>
+  </div>
+</template>
+
+<script>
+import NeoVis from 'neovis.js/dist/neovis.js';
+
+export default {
+  name: 'NeoVisExample',
+  mounted() {
+    this.draw();
+  },
+  methods: {
+    draw() {
+      const config = {
+        containerId: "viz",
+        neo4j: {
+          serverUrl: "bolt://localhost:7687",
+          serverUser: "neo4j",
+          serverPassword: "174235",
+        },
+        initialCypher: "MATCH (n)-[r]->(m) RETURN n, m"
+      };
+
+      try {
+        this.neoViz = new NeoVis(config);
+        this.neoViz.render();
+      } catch (error) {
+        console.error('Error initializing NeoVis:', error);
+      }
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+html, body {
+  font: 16pt arial;
+}
+
+#viz {
+  width: 900px;
+  height: 700px;
+  border: 1px solid lightgray;
+  font: 22pt arial;
+}
+</style>
